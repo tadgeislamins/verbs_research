ec6e92abc7913a1d35f442b6da18222d7a347e7b
Tasya <ttaisia2@gmail.com>
2020-04-23 20:46:14 +0300

    Предварительно доделана функция нахождения триплетов и прочее

--- notebooks/finding_my_triplets.py
+++ notebooks/finding_my_triplets.py
@@ -8,17 +8,20 @@ def read_conllu(conllu_fname): # reading conllu file with parsed sentences
 
 def check_object(token, object_form): # checking if form of object is
                                                     # identical with the form we search for this construction
-    if 'Foreign' in token['feats'] and token['feats']['Foreign'] == 'Yes':
+    if not token['feats']:
         return False
-    # print(object_form)
-    for feature in object_form:
-        if token['feats'][feature] != object_form[feature]:
+    else:
+        if 'Foreign' in token['feats'] and token['feats']['Foreign'] == 'Yes':
             return False
-    return True
+        # print(object_form)
+        for feature in object_form:
+            if token['feats'][feature] != object_form[feature]:
+                return False
+        return True
 
 
-def find_triplet_in_sentence(sent, verb, object_form, preposition=None, if_preposition=None):
-                                                        # finding triplet (verb, object, preposition) for one sentence
+def find_triplet_in_sentence(sent, verb, object_form, prep_in_var_of_constr=None, prep_in_constr=None):
+                                                        # finding triplet (verb, object, prep_in_var_of_constr) for one sentence
     # TODO: подумать про случаи, когда несколько подходящих глаголов в предложении
     triplet = {}
     verb_id = None
@@ -36,26 +39,32 @@ def find_triplet_in_sentence(sent, verb, object_form, preposition=None, if_prepo
                 object_id = token['id']
                 break  # мы нашли объект (TODO: bla bla)
 
-    if if_preposition and object_id:  # looking for a preposition, if there is
-        if preposition:
+    if prep_in_constr and object_id:  # looking for a preposition, if there is
+        if prep_in_var_of_constr:
             for token in sent:
-                if token['head'] == object_id and token['form'] == preposition:
+                if token['head'] == object_id and token['form'] == prep_in_var_of_constr:
                     triplet['object'] = sent[object_id - 1]['form']
-                    triplet['preposition'] = preposition
+                    triplet['preposition'] = prep_in_var_of_constr
         else:
             for token in sent:  # looking for a preposition, if there isn't
-                if token['head'] == object_id and token['form'] == preposition:
-                    break
+                if token['head'] == object_id:
+                    if token['form'] == prep_in_constr: # может быть, любой предлог искать ??
+                        break
+                    elif token['upostag']:
+                        if token['upostag'] == 'ADP':
+                            break
+
             else:
                 triplet['object'] = sent[object_id - 1]['form']
+
     return triplet
 
 
-def get_all_triples(sentences, verb, object_form, preposition=None, if_preposition=None):
+def get_all_triples(sentences, verb, object_form, prep_in_var_of_constr=None, prep_in_constr=None):
                                                             # finding triplet for all sentences (returns dictionary)
     triples = []
     for i, sent in enumerate(sentences):
-        triplet = find_triplet_in_sentence(sent, verb, object_form, preposition, if_preposition)
+        triplet = find_triplet_in_sentence(sent, verb, object_form, prep_in_var_of_constr, prep_in_constr)
         triplet['id'] = i
         triples.append(triplet)
     return triples
@@ -84,24 +93,31 @@ def get_standart_date(date):
         return date_array[0]
 
 if __name__ == '__main__':
-    sentences1 = read_conllu('parsed_sents_pisat_k_Dat.conllu')
+    sentences1 = read_conllu('parsed_sents_pisat_test_2-18.conllu')
     object_form = {'Case': 'Dat'} #, 'Animacy': 'Anim'}
     verb = 'писать'
-    preposition = 'к'
-    if_preposition = True
-    triplets1 = get_all_triples(sentences1, verb, object_form, preposition, if_preposition)
+    prep_in_var_of_constr = None
+    prep_in_constr = 'к'
+    triplets1 = get_all_triples(sentences1, verb, object_form, prep_in_var_of_constr, prep_in_constr)
 
-    sentences2 = read_conllu('parsed_sents_pisat_Dat.conllu')
-    object_form = {'Case': 'Dat'} #, 'Animacy': 'Anim'}
-    verb = 'писать'
-    preposition = False
-    if_preposition = True
-    triplets2 = get_all_triples(sentences2, verb, object_form, preposition, if_preposition)
+#    sentences2 = read_conllu('parsed_sents_pisat_Dat.conllu')
+#    object_form = {'Case': 'Dat'} #, 'Animacy': 'Anim'}
+#    verb = 'писать'
+#    prep_in_var_of_constr = False
+#    prep_in_constr = 'к'
+#    triplets2 = get_all_triples(sentences2, verb, object_form, prep_in_var_of_constr, prep_in_constr)
 
-    ids = get_indexes(triplets2)
+#    ids = get_indexes(triplets2)
 #     print(ids)
 
-#     for tr in triplets2:
-#         print(tr)
-    a = get_standart_date('1456-1876')
-    print(a)
+    for tr in triplets1:
+        print(tr)
+ #   a = get_standart_date('1456-1876')
+#    print(a)
+
+
+    print(count_triplets(triplets1))
+
+
+    print(sentences1[48])
+    print(sentences1[16])
